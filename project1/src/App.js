0b9fcc20a5f4116ad1e5d274caa7c57cbaf6c650
phonepyae1 <phonepyaekyawswar00@gmail.com>
2022-02-04 16:00:50 +0700

    added

--- project1/src/App.js
+++ project1/src/App.js
@@ -5,35 +5,92 @@ import {useRef} from "react";
 import QuotationTable from "./QuotationTable";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import useLocalStorage from 'react-localstorage-hook';
+import jasona from './cs-2019';
+
 
 function App() {
   const itemRef = useRef();
   const ppuRef = useRef();
   const qtyRef = useRef();
   const disRef = useRef();
+  const numRef = useRef();
+
 
   const [dataItems, setDataItems] = useLocalStorage("dataItems",[]);
 
   const dummyProductList = [
-    { id: "p000",name: "None" ,price: 0},
-    { id: "p001", name: "Samsung LED TV 52", price: 15000 },
-    { id: "p002", name: "Xiaomi Air Purifier", price: 4000 },
-    { id: "p003", name: "Brother Laserjet Printer", price: 5500 },
-    { id: "p004", name: "Bose Speaker", price: 20000 },
+    { id: "p000",name: jasona.curriculum.subjects[0].subjects[0].code ,price: jasona.curriculum.subjects[0].subjects[0].name},
+    { id: "p001",name: jasona.curriculum.subjects[0].subjects[1].code ,price: jasona.curriculum.subjects[0].subjects[1].name},
+    { id: "p002",name: jasona.curriculum.subjects[0].subjects[2].code ,price: jasona.curriculum.subjects[0].subjects[2].name},
+    { id: "p003",name: jasona.curriculum.subjects[0].subjects[3].code ,price: jasona.curriculum.subjects[0].subjects[3].name},
+    { id: "p004",name: jasona.curriculum.subjects[1].subjects[0].code ,price: jasona.curriculum.subjects[1].subjects[0].name},
+    { id: "p005",name: jasona.curriculum.subjects[2].subjects[0].code ,price: jasona.curriculum.subjects[2].subjects[0].name},
+    { id: "p006",name: jasona.curriculum.subjects[2].subjects[1].code ,price: jasona.curriculum.subjects[2].subjects[1].name},
+    { id: "p007",name: jasona.curriculum.subjects[3].subjects[0].code ,price: jasona.curriculum.subjects[3].subjects[0].name},
+    { id: "p008",name: jasona.curriculum.subjects[3].subjects[1].code ,price: jasona.curriculum.subjects[3].subjects[1].name},
+    { id: "p009",name: jasona.curriculum.subjects[4].subjects[0].code ,price: jasona.curriculum.subjects[4].subjects[0].name},
+    { id: "p010",name: jasona.curriculum.subjects[4].subjects[1].code ,price: jasona.curriculum.subjects[4].subjects[1].name},
+    { id: "p011",name: jasona.curriculum.subjects[4].subjects[2].code ,price: jasona.curriculum.subjects[4].subjects[2].name},
+    { id: "p012",name: jasona.curriculum.subjects[4].subjects[3].code ,price: jasona.curriculum.subjects[4].subjects[3].name},
+    { id: "p013",name: jasona.curriculum.subjects[4].subjects[4].code ,price: jasona.curriculum.subjects[4].subjects[4].name},
+    { id: "p014",name: jasona.curriculum.subjects[4].subjects[5].code ,price: jasona.curriculum.subjects[4].subjects[5].name},
+    { id: "p015",name: jasona.curriculum.subjects[4].subjects[6].code ,price: jasona.curriculum.subjects[4].subjects[6].name},
+    { id: "p016",name: jasona.curriculum.subjects[4].subjects[7].code ,price: jasona.curriculum.subjects[4].subjects[7].name},
+    { id: "p017",name: jasona.curriculum.subjects[4].subjects[8].code ,price: jasona.curriculum.subjects[4].subjects[8].name},
+    { id: "p018",name: jasona.curriculum.subjects[4].subjects[9].code ,price: jasona.curriculum.subjects[4].subjects[9].name},
+    { id: "p019",name: jasona.curriculum.subjects[4].subjects[10].code ,price: jasona.curriculum.subjects[4].subjects[10].name},
+    { id: "p020",name: jasona.curriculum.subjects[4].subjects[11].code ,price: jasona.curriculum.subjects[4].subjects[11].name},
+    { id: "p021",name: jasona.curriculum.subjects[4].subjects[12].code ,price: jasona.curriculum.subjects[4].subjects[12].name},
+    { id: "p022",name: jasona.curriculum.subjects[4].subjects[13].code ,price: jasona.curriculum.subjects[4].subjects[13].name},
+    { id: "p023",name: jasona.curriculum.subjects[4].subjects[14].code ,price: jasona.curriculum.subjects[4].subjects[14].name},
+    { id: "p024",name: jasona.curriculum.subjects[4].subjects[15].code ,price: jasona.curriculum.subjects[4].subjects[15].name},
+    { id: "p025",name: jasona.curriculum.subjects[4].subjects[16].code ,price: jasona.curriculum.subjects[4].subjects[16].name},
+    { id: "p026",name: jasona.curriculum.subjects[4].subjects[17].code ,price: jasona.curriculum.subjects[4].subjects[17].name},
+    { id: "p027",name: jasona.curriculum.subjects[4].subjects[18].code ,price: jasona.curriculum.subjects[4].subjects[18].name},
+    { id: "p028",name: jasona.curriculum.subjects[4].subjects[19].code ,price: jasona.curriculum.subjects[4].subjects[19].name},
+    { id: "p029",name: jasona.curriculum.subjects[4].subjects[20].code ,price: jasona.curriculum.subjects[4].subjects[20].name},
+    { id: "p030",name: jasona.curriculum.subjects[4].subjects[21].code ,price: jasona.curriculum.subjects[4].subjects[21].name},
+    { id: "p031",name: jasona.curriculum.subjects[4].subjects[22].code ,price: jasona.curriculum.subjects[4].subjects[22].name},
+    { id: "p032",name: jasona.curriculum.subjects[4].subjects[23].code ,price: jasona.curriculum.subjects[4].subjects[23].name},
+    { id: "p033",name: jasona.curriculum.subjects[4].subjects[24].code ,price: jasona.curriculum.subjects[4].subjects[24].name},
+    { id: "p034",name: jasona.curriculum.subjects[4].subjects[25].code ,price: jasona.curriculum.subjects[4].subjects[25].name},
+    { id: "p035",name: jasona.curriculum.subjects[4].subjects[26].code ,price: jasona.curriculum.subjects[4].subjects[26].name},
+    { id: "p036",name: jasona.curriculum.subjects[4].subjects[27].code ,price: jasona.curriculum.subjects[4].subjects[27].name},
+    { id: "p037",name: jasona.curriculum.subjects[4].subjects[28].code ,price: jasona.curriculum.subjects[4].subjects[28].name},
+
+
   ];
 
+  const gradeList = [ 
+    {id:1, grade: "A"},
+    {id:2,grade: "A-"},
+    {id:3,grade: "B+"},
+    {id:4,grade: "B"},
+    {id:5,grade: "B-"},
+    {id:6,grade: "C+"},
+    {id:7,grade: "C"},
+    {id:8,grade: "C-"},
+    {id:9,grade: "D"},
+    {id:10,grade: "F"},
+    {id:11,grade: "Else"},
+  ];
+
+  const gpaList = [ 
+    {id:"A",grade1: 4},
+    {id:"A-",grade1: 3.75},
+    {id:"B+",grade1: 3.25},
+    {id:"B",grade1: 3},
+    {id:"B-",grade1: 2.75},
+    {id:"C+",grade1: 2.5},
+    {id:"C",grade1: 2},
+    {id:"C-",grade1: 1.75},
+    {id:"D",grade1: 1.5},
+    {id:"F",grade1: 0},
+  ];
 
   const addItem = () => {
-    if (qtyRef.current.value === "") {
-      alert("Quantity cannot be empty")
-      return;
-    }
 
-    if (qtyRef.current.value === "0") {
-      alert("Quantity cannot be empty")
-      return;
-    }
-
+    console.log(dataItems.length)
     if (itemRef.current.value === "") {
       alert("Item name is empty");
       return;
@@ -43,10 +100,6 @@ function App() {
       return;
     }
 
-    if (disRef.current.value === "") {
-      disRef.current.value = 0
-    }
-
     const pid = itemRef.current.value;
     const product = dummyProductList.find(e => e.id === pid);
 
@@ -54,59 +107,102 @@ function App() {
       alert("Please add a valid item!");
       return;
     }
-
+  
+  
     if (Object.keys(dataItems).length > 0) {
+     // console.log(dataItems[0].item.includes(product.name));
+     // console.log(dataItems[0].item);
+
+      
       for (var key in dataItems) {
-        if (product.name === dataItems[key].item) {
+        var string = product.name
+        var str1 = dataItems[key].item
+        if (str1.includes(string)) {
+          alert("Cannot add the same course !!! ");
+          return;
+        }
         
-          //dataItems[key].ppu = parseFloat(ppuRef.current.value) + parseFloat(dataItems[key].ppu)
-          dataItems[key].qty =  parseFloat(qtyRef.current.value) + parseFloat(dataItems[key].qty)
-          dataItems[key].dis = parseFloat(disRef.current.value) + parseFloat(dataItems[key].dis)
-          setDataItems([...dataItems]);
+
+        if (qtyRef.current.value === dataItems[key].qty) {
+        
+          dataItems[key].item = dataItems[key].item +" , "+  product.name
+          dataItems[key].ppu = dataItems[key].ppu +" , "+  ppuRef.current.value
+          dataItems[key].dis = dataItems[key].dis +" , "+  disRef.current.value
+          var str = dataItems[key].dis;
+          var count = (str.match(/\,/g) || []).length;
+          console.log(","+ count)
+          console.log(dataItems[key].num);
+          console.log(numRef.current.value);
+          dataItems[key].num = (parseFloat(dataItems[key].num) + parseFloat(numRef.current.value)).toFixed(2); 
+          console.log(dataItems[key].num);
+          setDataItems([...dataItems])
           return;
         }
       }
     }
       
     var itemObj = {
+      qty: qtyRef.current.value,
       item: product.name,
       ppu: ppuRef.current.value,
-      qty: qtyRef.current.value,
       dis: disRef.current.value,
+      num: numRef.current.value
     };
 
     dataItems.push(itemObj);
     setDataItems([...dataItems]);
-    console.log(dataItems[1].item);
-    console.log(itemRef.current.value);
-    console.log(dataItems);
+    //console.log(itemRef.current.value);
+    //console.log(dataItems);
+    //console.log("numRef" + numRef)
     // setDataItems(dataItems)
-
+  
   };
 
+
+
   const productChange = (e) => {
+    console.log(disRef.current.value)
     const pid = itemRef.current.value;
     const product = dummyProductList.find((e) => e.id === pid);
     ppuRef.current.value = product.price
-    
+  }
+  
+  const PproductChange = (e) => {
+    console.log(disRef.current.value);
+    const pidd = disRef.current.value;
+    const product1 = gpaList.find((e) => e.id === pidd);
+    console.log(product1.grade1)
+    numRef.current.value = product1.grade1
   }
   
   const options = dummyProductList.map(v => {
     return <option value={v.id}>{v.name}</option>
   })
 
+  const options1 = gradeList.map(v => {
+    return <option>{v.grade}</option>
+  })
+
+
+
   return (
     
     <>
     <div className="App-header">
-      <h1><img src={logo} alt="logo" height={50}/> Midterm-6118156 </h1>
+      <h1> GradeTracker </h1>
     </div>
     <Container>
         <Row>
-          <Col xs={4} style={{ backgroundColor: '#B53737' }}>
+          <Col xs={4} style={{ backgroundColor: '#104a8e' }}>
             <Form>
+
+            <Form.Group className="mb-3" controlId="formQauntity">
+                <Form.Label>Semester</Form.Label>
+                <Form.Control min="0"  placeholder="1 or 2" ref={qtyRef}/>
+              </Form.Group>
+
               <Form.Group className="mb-3" controlId="formItem">
-                <Form.Label>Item</Form.Label>
+                <Form.Label>Code</Form.Label>
                 <Form.Select
 
                   aria-label="Default select example"
@@ -117,26 +213,29 @@ function App() {
                 </Form.Select>
               </Form.Group>
 
-              <Form.Group className="mb-3" controlId="formPrice">
-                <Form.Label>Price</Form.Label>
-                <Form.Control
-                disabled={true}
-                  type="number"
-                  placeholder="Price Per Unit"
-                  ref={ppuRef} />
-              </Form.Group>
-
               <Form.Group className="mb-3" controlId="formQauntity">
-                <Form.Label>Quantity</Form.Label>
-                <Form.Control min="0" type="number" placeholder="Quantity" ref={qtyRef} />
+                <Form.Label>Subjects</Form.Label>
+                <Form.Control disabled= "true" min="0"  placeholder="Subjects" ref={ppuRef}  onChange={productChange}/>
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formDiscount">
-                <Form.Label>Discount</Form.Label>
-                <Form.Control min="0" type="number" placeholder="Discount" ref={disRef} />
+                <Form.Label>Grade</Form.Label>
+                <Form.Select
+
+                  aria-label="Default select example"
+                  ref={disRef}
+                  onChange={PproductChange}
+                >
+                  {options1}
+                </Form.Select>
               </Form.Group>
 
-              <Button variant="dark" onClick={addItem} margin>
+              <Form.Group className="mb-3" controlId="formGPA">
+                <Form.Label>GPA</Form.Label>
+                <Form.Control disabled= "true" min="0"  placeholder="GPA" ref={numRef}  onChange={PproductChange}/>
+              </Form.Group>
+
+              <Button variant="secondary" onClick={addItem} margin>
                 Add
               </Button>
             </Form>
--- project1/src/QuotationTable.js
+++ project1/src/QuotationTable.js
@@ -9,33 +9,60 @@ const styles = {
 function QuotationTable({ data, setDataItems }) {
   const [dataRows, setDataRows] = useState();
   const [totalPrice, setTotalPrice] = useState(0);
-  const [totalDiscount, setTotalDiscount] = useState(0);
 
+  
+  
   useEffect(() => {
     let sum = 0;
-    let discount = 0;
+    let sum1 = 0;
+    let total = 0;
+    let count = 0;
+
+    
+
     const z = data.map((v, i) => {
+
+      var str = v.dis;
+      var count = (str.match(/\,/g) || []).length;
+      let amount = v.num/(count+1);
+
+      if (data.length > 0) {
+            var str1 = v.dis;
+            var count1 = (str1.match(/\,/g) || []).length;
+            console.log(v.num+ " "+ count1);
+             sum =(parseFloat(v.num))/(count1+1);
+             console.log("sum "+sum);
+             console.log("sum1 "+sum1);
+             sum1 = sum1+ sum;
+             console.log("sum1 "+sum1);
+             console.log("asfasd" + v.num);
+      }
       
-      let disco = parseFloat(v.dis);
-      let amount = (v.qty * v.ppu)- disco;
-      discount += disco;
-      sum += amount;
-      
+        
+      if (v.num.toString().length > 4) {
+        amount = amount.toFixed(2);
+      }
+      console.log(data.length)
+      console.log("sum1 "+sum1);
+      total = sum1/(data.length)
+      console.log(total)
+      // <td style={styles.textRight}>{v.dis}</td>
+      //  <th style={styles.textRight}>{Math.round(totalDiscount * 10) / 10}</th>
       return (
         <tr key={i}>
             <td><FaTrash onClick={() => deleteClick(i)}/></td>
-            <td style={styles.textCenter}>{v.qty}</td>
-            <td>{v.item}</td>
-            <td style={styles.textRight}>{numberWithCommas(v.ppu)}</td>
-            <td style={styles.textRight}>{v.dis}</td>
-            <td style={styles.textRight}>{numberWithCommas(amount)}</td>
+            <td>{v.qty}</td>
+            <td style={styles.textCenter}>{v.item}</td>
+            <td style={styles.textRight}>{v.ppu}</td>
+            <td style={styles.textRight}>{numberWithCommas(v.dis)}</td>
+            <td style={styles.textRight}>{amount}</td>
         </tr>
       );
     });
 
     setDataRows(z);
-    setTotalPrice(sum);
-    setTotalDiscount(discount);
+    setTotalPrice(total);
+   
   }, [data]);
 
   const deleteClick = (i) => {
@@ -59,30 +86,29 @@ function QuotationTable({ data, setDataItems }) {
         <h1>Quotation Table</h1>
       </Col>
       <Col style={styles.textRight}>
-        <Button onClick={clearTable} variant="dark">
+        <Button onClick={clearTable} variant="secondary">
           Clear
         </Button>
       </Col>
     </Row>
     <Table striped bordered hover>
-      <thead style={{backgroundColor:'#B53737'}}>
+      <thead style={{backgroundColor:'#104a8e'}}>
         <tr>
           <th></th>
-          <th>Qty</th>
-          <th>Item</th>
-          <th>Price/Unit</th>
-          <th>Discount</th>
-          <th>Amount</th>
+          <th>Semester</th>
+          <th>Code</th>
+          <th>Subjects</th>
+          <th>Grade</th>
+          <th>Sem GPA</th>
     
         </tr>
       </thead>
       <tbody>{dataRows}</tbody>
       <tfoot>
           <tr>
-            <th colSpan={3}></th>
-            <th style={styles.textCenter}>Total</th>
-            <th style={styles.textRight}>{Math.round(totalDiscount * 10) / 10}</th>
-            <th style={styles.textRight}>{numberWithCommas(totalPrice)}</th>
+            <th colSpan={4}></th>
+            <th style={styles.textCenter}>Your GPA</th>
+            <th style={styles.textRight}>{numberWithCommas(totalPrice.toFixed(2))}</th>
           </tr>
         
       </tfoot>
